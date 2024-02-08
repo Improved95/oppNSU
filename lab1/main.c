a81f520443d3295df13ab9758d7ce37f2ff00608
e.davidov@ <e.davidov@g.nsu.ru>
2024-02-08 13:29:09 +0700

    l1 initial

--- lab1/main.c
+++ lab1/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define N 100
+static const double epsilon = 1 / 10 * 10 * 10 * 10 * 10;
+
+void setMatrix(double *matrix) {
+	for (size_t i = 0; i < N * N; ++i) {
+		matrix[i] = 1.0;
+	}
+	for (size_t i = 0; i < N * N; i += N) {
+		matrix[i] = 2.0;
+ 	}
+}
+
+int charIsAchieve() {
+	return 1;
+}
 
 int main() {
-	
+	double *matrix = malloc(N * N * sizeof(double));
+	double *vectorA = malloc(N * sizeof(double));
+	double *vectorB = malloc(N * sizeof(double));
+
+	while (!charIsAchieve()) {
+
+	}
 
 	return 0;
 }
