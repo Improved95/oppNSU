9d0d37ee4e5770bbc4bc6285ed897a3bdb551bc1
e.davidov@ <e.davidov@g.nsu.ru>
2024-02-08 13:41:41 +0700

    created inital matrix

--- lab1/main.c
+++ lab1/main.c
@@ -2,14 +2,23 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define N 100
+#define N 10
 static const double epsilon = 1 / 10 * 10 * 10 * 10 * 10;
+
+void printMatrix(double *matrix) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			printf("%f ", matrix[i * N + j]);
+		}
+		printf("\n");
+	}
+}
 
 void setMatrix(double *matrix) {
 	for (size_t i = 0; i < N * N; ++i) {
 		matrix[i] = 1.0;
 	}
-	for (size_t i = 0; i < N * N; i += N) {
+	for (size_t i = 0; i < N * N; i += N + 1) {
 		matrix[i] = 2.0;
  	}
 }
@@ -22,6 +31,8 @@ int main() {
 	double *matrix = malloc(N * N * sizeof(double));
 	double *vectorA = malloc(N * sizeof(double));
 	double *vectorB = malloc(N * sizeof(double));
+	setMatrix(matrix);
+	printMatrix(matrix);
 
 	while (!charIsAchieve()) {
 
