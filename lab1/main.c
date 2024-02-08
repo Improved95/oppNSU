73c8aa925eb6aba5f5f0917fada05a47f72bef60
e.davidov@ <e.davidov@g.nsu.ru>
2024-02-08 22:09:53 +0700

    l1

--- lab1/main.c
+++ lab1/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define M_PI 3.14159265358979323846
 
 #define N 10
 static const double epsilon = 1 / 10 * 10 * 10 * 10 * 10;
 
+// Ax = b
+
 void printMatrix(double *matrix) {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
+	for (size_t i = 0; i < N; ++i) {
+		for (size_t j = 0; j < N; ++j) {
 			printf("%f ", matrix[i * N + j]);
 		}
 		printf("\n");
@@ -23,16 +26,25 @@ void setMatrix(double *matrix) {
  	}
 }
 
+void setVectorU(double *vector) {
+	for (size_t i = 0; i < N; ++i) {
+		vector[i] = sin((2 * M_PI * i) / N);
+	}
+}
+
 int charIsAchieve() {
 	return 1;
 }
 
 int main() {
 	double *matrix = malloc(N * N * sizeof(double));
-	double *vectorA = malloc(N * sizeof(double));
-	double *vectorB = malloc(N * sizeof(double));
+	double *vectorA = calloc(sizeof(double), N);
+	double *vectorB = calloc(sizeof(double), N);
+	double *vectorU = calloc(sizeof(double), N);
+
 	setMatrix(matrix);
-	printMatrix(matrix);
+	setVectorU(vectorU);
+	
 
 	while (!charIsAchieve()) {
 
