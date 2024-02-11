681b681d5d08e4cf12647455139a81445388ef1d
e.davydov <e.davydov@g.nsu.ru>
2024-02-11 20:03:34 +0700

    безпараллелка работает

--- lab1/main.c
+++ lab1/main.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 #define PI 3.14159265358979323846
-#define N 3
+#define N 10
 
 // Ax = b
 
@@ -33,7 +33,7 @@ void printVector(double *vector) {
     // }
 
 void setZeroVector(double *vector) {
-	memset(vector, N, sizeof(double));
+	memset(vector, 0, N * sizeof(double));
 }
 
 void subVector(double *vector1, double *vector2) {
@@ -45,16 +45,15 @@ void subVector(double *vector1, double *vector2) {
 double scalarMul(double *vector1, double *vector2) {
 	double result = 0;
 	for (size_t i = 0; i < N; ++i) {
-		result += (vector1[i] + vector2[i]);
+		result += (vector1[i] * vector2[i]);
 	}
-
 	return result;
 }
 
 void mulMatrixVector(double *matrix, double *inputVector, double *outputVector) {
 	for (size_t i = 0; i < N; ++i) {
 		for (size_t j = 0; j < N; ++j) {
-			outputVector[j] += matrix[i * N + j] * inputVector[j];
+			outputVector[i] += matrix[i * N + j] * inputVector[j];
 		}
 	}
 }
@@ -64,7 +63,7 @@ void mulVectorVector(double *vector1, double *vector2) {
 }
 
 int main() {
-	static const double epsilon = 0.00001;
+	const double epsilon = 0.00001;
 
 	double *matrixA = malloc(N * N * sizeof(double));
 	for (size_t i = 0; i < N * N; ++i) {
@@ -78,47 +77,56 @@ int main() {
 	for (size_t i = 0; i < N; ++i) {
 		vectorU[i] = sin(2 * 3.14 * (i + 1) / N);
 	}
+	printVector(vectorU);
+	printf("\n");
 
 	double *vectorX = calloc(sizeof(double), N);
 	double *vectorB = calloc(sizeof(double), N);
 
-	double *vectorE = calloc(sizeof(double), N); // y^n
-	double *vectorBuffer = calloc(sizeof(double), N); 
-	double tao = 0;
+	double *vectorAxn_b = calloc(sizeof(double), N);
+	double *vectorAyn = calloc(sizeof(double), N); 
+	double tao = 0, t1 = 0, t2 = 0;
 
 	//take vector b
 	setZeroVector(vectorB);
 	mulMatrixVector(matrixA, vectorU, vectorB);
-	printVector(vectorU);
-	printVector(vectorB);
-	exit(1);
-	
-	while(1) {
-		setZeroVector(vectorE);
-		setZeroVector(vectorBuffer);
-		mulMatrixVector(matrixA, vectorX, vectorE);
-		subVector(vectorE, vectorB);
+
+	for(size_t k = 0; 1; ++k) {
+		setZeroVector(vectorAxn_b);
+		mulMatrixVector(matrixA, vectorX, vectorAxn_b);
+		subVector(vectorAxn_b, vectorB);
+
 		double numerator = 0, denominator = 0;
+
 		for (size_t i = 0; i < N; ++i) {
-			numerator += pow(vectorE[i], 2);
+			double a = vectorAxn_b[i];
+			numerator += (a * a);
 		}
 		numerator = sqrt(numerator);
+
 		for (size_t i = 0; i < N; ++i) {
-			denominator += pow(vectorB[i], 2);
+			double a = vectorB[i];
+			denominator += (a * a);
 		}
 		denominator = sqrt(denominator);
+
 		if (numerator / denominator < epsilon) {
 			break;
 		}
+		// printf("%f %f\n", numerator, denominator);
 
-		mulMatrixVector(matrixA, vectorE, vectorBuffer);
-		double t1 = scalarMul(vectorE, vectorBuffer);
-		double t2 = scalarMul(vectorBuffer, vectorBuffer);
+		setZeroVector(vectorAyn);
+		mulMatrixVector(matrixA, vectorAxn_b, vectorAyn);
+		t1 = scalarMul(vectorAxn_b, vectorAyn);
+		t2 = scalarMul(vectorAyn, vectorAyn);
 		tao = t1 / t2;
 
 		for (size_t i = 0; i < N; ++i) {
-			vectorX[i] = vectorX[i] - (vectorE[i] * tao);
+			vectorX[i] = vectorX[i] - (vectorAxn_b[i] * tao);
 		}
+
+		// printVector(vectorX);
+		// if (k == 1000) exit(-1);
 	}
 
 	printVector(vectorX);
@@ -130,3 +138,24 @@ int main() {
 
 	return 0;
 }
+
+// int main() {
+// 	double *matrixA = malloc(N * N * sizeof(double));
+// 	for (size_t i = 0; i < N * N; ++i) {
+// 		matrixA[i] = 1.0;
+// 	}
+// 	for (size_t i = 0; i < N * N; i += N + 1) {
+// 		matrixA[i] = 2.0;
+//  	}
+
+// 	double *vectorX = calloc(sizeof(double), N);
+// 	double *vectorR = calloc(sizeof(double), N);
+// 	vectorX[0] = 5;
+// 	vectorX[1] = 6;
+// 	vectorX[2] = 10;
+
+// 	mulMatrixVector(matrixA, vectorX, vectorR);
+// 	printVector(vectorR);
+
+// 	return 0;
+// }
