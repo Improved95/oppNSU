c9c817d2b0b1a584c5879ea2f2753df370f0c51e
e.davidov@ <e.davidov@g.nsu.ru>
2024-02-13 14:05:05 +0700

    l1

--- lab1/mpi.c
+++ lab1/mpi.c
@@ -2,18 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <mpi.h>
 
 #define PI 3.14159265358979323846
 #define N 10
-
-// for (size_t i = 0; i < N; i++) {
-    //     for (size_t k = 0; k < N; k++) {
-    //         for (size_t j = 0; j < N; j++) {
-    //             temp[i][j] += (*this)[i][k] * source[k][j];
-	// 		}
-    //     }
-    // }
-// Ax = b
+static int rank, sizeProc;
 
 void printMatrix(double *matrix) {
 	for (size_t i = 0; i < N; ++i) {
@@ -51,9 +44,11 @@ double scalarMul(double *vector1, double *vector2) {
 
 void mulMatrixVector(double *matrix, double *inputVector, double *outputVector) {
 	for (size_t i = 0; i < N; ++i) {
-		for (size_t j = 0; j < N; ++j) {
-			outputVector[i] += matrix[i * N + j] * inputVector[j];
-		}
+			
+			for (size_t j = 0; j < N; ++j) {
+				outputVector[i] += matrix[i * N + j] * inputVector[j];
+			}
+
 	}
 }
 
@@ -61,34 +56,51 @@ void mulVectorVector(double *vector1, double *vector2) {
 
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &sizeProc);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
 	const double epsilon = 0.00001;
 
-	double *matrixA = malloc(N * N * sizeof(double));
-	for (size_t i = 0; i < N * N; ++i) {
-		matrixA[i] = 1.0;
+	double *matrixA = NULL;
+	if (rank == 0) {
+
+		matrixA = сalloc(N * N, sizeof(double));
+		for (size_t i = 0; i < N; ++i) {
+			matrixA[i] = 1.0;
+			for (size_t j = 0; i < N; ++j) {
+				matrixA[i] = 2.0;
+			}
+		}
+
 	}
-	for (size_t i = 0; i < N * N; i += N + 1) {
-		matrixA[i] = 2.0;
- 	}
 
 	double *vectorU = calloc(sizeof(double), N);
-	for (size_t i = 0; i < N; ++i) {
-		vectorU[i] = sin(2 * PI * (i + 1) / N);
+	if (rank == 0) {
+
+		for (size_t i = 0; i < N; ++i) {
+			vectorU[i] = sin(2 * PI * (i + 1) / N);
+		}
+		printVector(vectorU);
+		printf("\n");
+
 	}
-	printVector(vectorU);
-	printf("\n");
 
 	double *vectorX = calloc(sizeof(double), N);
 	double *vectorB = calloc(sizeof(double), N);
 
 	double *vectorAxn_b = calloc(sizeof(double), N);
-	double *vectorAyn = calloc(sizeof(double), N); 
+	double *vectorAyn = calloc(sizeof(double), N);
 	double tao = 0, t1 = 0, t2 = 0;
 
-	setZeroVector(vectorB);
-	mulMatrixVector(matrixA, vectorU, vectorB);
+	if (rank == 0) {
 
+		setZeroVector(vectorB);
+		mulMatrixVector(matrixA, vectorU, vectorB);
+
+	}
+	
 	for(size_t k = 0; 1; ++k) {
 		setZeroVector(vectorAxn_b);
 		mulMatrixVector(matrixA, vectorX, vectorAxn_b);
--- lab1/test.c
+++ lab1/test.c
@@ -2,16 +2,30 @@
 #include <stdlib.h>
 #include <mpi.h>
 
+static int x;
+
 int main(int argc, char *argv[]) {
     int rank, size;
 
     MPI_Init(&argc, &argv);
+    MPI_Status st;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    if (size >= 2) {
+        if (rank == 0) {
+            x = 10;
+            // MPI_Send(&x, 1, MPI_INT, 1, 01, MPI_COMM_WORLD);
+        }
+        if (rank == 1) {
+            x = 5;
+            // MPI_Recv(&x, 1, MPI_INT, 0, 01, MPI_COMM_WORLD, &st);
+        }
+    }
+
     MPI_Finalize();
 
-    printf("Process: %d, size: %d\n", rank, size);
+    printf("Process: %d, size: %d, x: %d\n", rank, size, x);
 
     return 0;
 }
