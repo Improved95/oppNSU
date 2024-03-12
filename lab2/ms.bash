d2e1ad4147d85dedee3e7c75b45ccca9db438495
e.davidov@ <e.davidov@g.nsu.ru>
2024-03-12 13:42:06 +0700

    v1 complete

--- lab2/ms.bash
+++ lab2/ms.bash
@@ -1,2 +1,5 @@
-gcc openMPv1.c -lm -Wall -Werror -Wextra -O3 -o siov1.out
-./siov1.out
+gcc native.c -lm -Wall -Werror -Wextra -O3 -o native.out
+time ./native.out
+
+gcc openMPv$1.c -lm -fopenmp -Wall -Werror -Wextra -O3 -o siov$1.out
+time ./siov$1.out
