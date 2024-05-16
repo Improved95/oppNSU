aacea3f8e1c534e4a849cf2da20c4bd59223ae76
e.davydov-server <e.davydov@g.nsu.ru>
2024-05-16 15:06:03 +0700

    l5

--- lab5/target/CMakeFiles/corelib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/corelib.dir/queue.c.o"
-  "CMakeFiles/corelib.dir/queue.c.o.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/corelib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
