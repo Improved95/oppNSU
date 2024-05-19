407792a6ee6763f188b8cf3b9614e8fc513ff28d
e.davydov-server <e.davydov@g.nsu.ru>
2024-05-19 18:15:44 +0700

    l5

--- lab5/cluster.c
+++ lab5/cluster.c
@@ -6,9 +6,104 @@
 #include <unistd.h>
 #include <mpi.h>
 #include <pthread.h>
+#include <stdbool.h>
 
-#include "task_queue.h"
 #include "color.h"
+
+/* TASK QUEUE */
+
+#define SUCCESS 0
+#define ERROR   (-1)
+
+typedef struct {
+    int id;
+    int process_id;
+    int weight;
+} Task;
+
+typedef struct {
+    Task *task_list;
+    int capacity;
+    int size;
+    int pop_index;
+} Task_Queue;
+
+Task_Queue *task_queue_create(int capacity) {
+    Task_Queue *task_queue = malloc(sizeof(Task_Queue));
+
+    if (task_queue == NULL) {
+        return NULL;
+    }
+
+    Task *task_list = malloc (sizeof(Task) * capacity);
+    if (task_list == NULL) {
+        return NULL;
+    }
+
+    task_queue->task_list = task_list;
+    task_queue->capacity = capacity;
+    task_queue->size = 0;
+    task_queue->pop_index = 0;
+
+    return task_queue;
+}
+
+bool task_queue_is_empty(const Task_Queue *task_queue) {
+    return task_queue->size == 0;
+}
+
+bool task_queue_is_full(const Task_Queue *task_queue) {
+    return task_queue->size == task_queue->capacity;
+}
+
+int task_queue_push(Task_Queue *task_queue, Task task) {
+    if (task_queue == NULL) {
+        return ERROR;
+    }
+
+    if (task_queue_is_full(task_queue)) {
+        return ERROR;
+    }
+
+    int push_index = (task_queue->pop_index + task_queue->size) % task_queue->capacity;
+    task_queue->task_list[push_index] = task;
+    task_queue->size++;
+
+    return SUCCESS;
+}
+
+int task_queue_pop(Task_Queue *task_queue, Task *task) {
+    if (task_queue == NULL) {
+        return ERROR;
+    }
+
+    if (task_queue_is_empty(task_queue)) {
+        return ERROR;
+    }
+
+    *task = task_queue->task_list[task_queue->pop_index];
+    task_queue->pop_index = (task_queue->pop_index + 1) % task_queue->capacity;
+    task_queue->size--;
+    
+    return SUCCESS;
+}
+
+void task_queue_destroy(Task_Queue **task_queue) {
+    if (*task_queue == NULL) {
+        return;
+    }
+
+    if ((*task_queue)->task_list == NULL) {
+        return;
+    }
+
+    free((*task_queue)->task_list);
+    free(*task_queue);
+
+    *task_queue = NULL;
+}
+
+/* TASK QUEUE */
 
 #define TASK_COUNT              2000
 #define TOTAL_SUM_WEIGHT        50000000 * 1.5
