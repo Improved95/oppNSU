17d81c864cdf19cd16b076d0459589c0386e000e
e.davydov-server <e.davydov@g.nsu.ru>
2024-05-20 14:15:16 +0700

    l5

--- lab5/task.c
+++ lab5/task.c
@@ -9,9 +9,23 @@ int main() {
     FILE *file = fopen("task.txt", "w");
 
     int min_weight = 2 * TOTAL_SUM_WEIGHT / (TASK_COUNT * (PROCESS_COUNT + 1));
-    printf("%d", min_weight);
 
+    // for (int i = 0; i < TASK_COUNT; ++i) {
+        // fprintf(file, "%d %d\n", i + 1, min_weight * (i % PROCESS_COUNT + 1));
+    // }
+
+    int summary_weigh_1 = 0;
+    int task_weight_process[PROCESS_COUNT] = {0};
     for (int i = 0; i < TASK_COUNT; ++i) {
-        fprintf(file, "%d %d\n", i + 1, min_weight * (i % PROCESS_COUNT + 1));
+        task_weight_process[i % PROCESS_COUNT] += min_weight * (i % PROCESS_COUNT + 1);
+        summary_weigh_1 += min_weight * (i % PROCESS_COUNT + 1);
     }
+
+    int summary_weigh_2 = 0;
+    for (int i = 0; i < 16; ++i) {
+        printf("%d\n", task_weight_process[i]);
+        summary_weigh_2 += task_weight_process[i];
+    }
+
+    printf("%d %d", summary_weigh_1, summary_weigh_2);
 }
